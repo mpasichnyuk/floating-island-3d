6cc5b343371130937f4ca697d25ab8897c0a928b
Mikhail <maikl@Mikhails-MacBook-Pro.local>
2024-04-08 13:58:41 -0400

    create index file for components and import them into router

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 
+import { About, Contacts, Home, Projects } from "./pages";
+
 const App = () => {
   return (
     <main className="bg-slate-300/20">
       <Router>
         <Navbar />
         <Routes>
-          <Route path="/" Component={"Home"} />
-          <Route path="/about" Component={"About"} />
-          <Route path="/projects" Component={"Projects"} />
-          <Route path="/contact" Component={"Contact"} />
+          <Route path="/" Component={<Home />} />
+          <Route path="/about" Component={<About />} />
+          <Route path="/projects" Component={<Projects />} />
+          <Route path="/contact" Component={<Contacts />} />
         </Routes>
       </Router>
     </main>
---
+++ src/pages/index.js
@@ -0,0 +1,6 @@
+import About from "./About";
+import Contacts from "./Contacts";
+import Home from "./Home";
+import Projects from "./Projects";
+
+export { About, Contacts, Home, Projects };
