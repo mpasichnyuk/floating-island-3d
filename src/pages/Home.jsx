fd893aeb671752d976e86d30f223f2bb69ccffb1
Mikhail <maikl@Mikhails-MacBook-Pro.local>
2024-04-08 15:46:27 -0400

    create a react component for 3d scene and add it to home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,28 @@
+import { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import Loader from "../components/Loader";
+import Island from "../models/Island";
+
 const Home = () => {
   return (
     <section className="w-full h-screen relative">
+      <Canvas
+        className="w-full h-screen bg-transparent"
+        camera={{
+          near: 0.1,
+          far: 1000,
+        }}
+      >
+        <Suspense fallback={<Loader />}>
+          <directionalLight />
+          <ambientLight />
+          <Island />
+        </Suspense>
+      </Canvas>
+      {/*       
       <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
-        POPUP HERE
-      </div>
+        // POPUP HERE //{" "}
+      </div> */}
     </section>
   );
 };
