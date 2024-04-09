4058083db4dee1e8dee067f45fa5fda720e9dec7
Mikhail <maikl@Mikhails-MacBook-Pro.local>
2024-04-09 11:32:53 -0400

    use plane animation when isRotating true

--- src/models/Plane.jsx
+++ src/models/Plane.jsx
@@ -1,12 +1,28 @@
-import { useGLTF } from "@react-three/drei";
+import { useRef, useEffect } from "react";
+import { useGLTF, useAnimations } from "@react-three/drei";
 import planeModel from "../assets/3d/cartoon_plane.glb";
+import { act } from "react-dom/test-utils";
 
 const Plane = (props) => {
   const { isRotating } = props;
+  const ref = useRef();
   const { scene, animations } = useGLTF(planeModel);
+  const { actions } = useAnimations(animations, scene);
+
+  useEffect(() => {
+    if (isRotating) {
+      actions["Main"].play();
+    } else {
+      actions["Main"].stop();
+    }
+
+    return () => {
+      // second;
+    };
+  }, [actions, isRotating]);
 
   return (
-      <mesh {...props}>
+    <mesh {...props} ref={ref}>
       <primitive object={scene} />
     </mesh>
   );
