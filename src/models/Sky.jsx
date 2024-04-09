580a089082aa697b620b4084f4dd5f590cdd6e82
Mikhail <maikl@Mikhails-MacBook-Pro.local>
2024-04-09 12:31:50 -0400

    animate sky on rotating

--- src/models/Sky.jsx
+++ src/models/Sky.jsx
@@ -1,11 +1,20 @@
+import { useRef } from "react";
 import { useGLTF } from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
 import skyScene from "../assets/3d/sky.glb";
 
-const Sky = () => {
+const Sky = ({ isRotating }) => {
+  const skyRef = useRef();
   const sky = useGLTF(skyScene);
 
+  useFrame((_, delta) => {
+    if (isRotating) {
+      skyRef.current.rotation.y += 0.15 * delta;
+    }
+  });
+
   return (
-    <mesh>
+    <mesh ref={skyRef}>
       <primitive object={sky.scene} />
     </mesh>
   );
