4facb6251ca6f5d293469f7ea3852d70fcbda243
Mikhail <maikl@Mikhails-MacBook-Pro.local>
2024-04-10 16:02:46 -0400

    create contacts page form

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -1,5 +1,94 @@
+import { useState, useRef } from "react";
+import emailjs from "@emailjs/browser";
 const Contacts = () => {
-  return <div>Contacts</div>;
+  const [isLoading, setIsLoading] = useState(false);
+  const formRef = useRef(null);
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+
+    emailjs.sendForm("service_id", "account_id");
+  };
+
+  const handleChange = (e) => {
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value,
+    });
+  };
+  const handleFocus = () => {};
+  const handleBlur = () => {};
+
+  return (
+    <section className="relative flex lg:flex-row flex-col max-container">
+      <div className="flex-1 min-w[50%] flex flex-col">
+        <h1 className="head-text">Get in touch!</h1>
+        <form
+          onSubmit={handleSubmit}
+          className="w-full flex flex-col gap-7 mt-14"
+          action=""
+        >
+          <label htmlFor="" className="text-black-500 font-semibold">
+            name
+            <input
+              type="text"
+              name="name"
+              className="input"
+              placeholder="John"
+              required
+              value={form.name}
+              onChange={handleChange}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+            />
+          </label>
+          <label htmlFor="" className="text-black-500 font-semibold">
+            Email
+            <input
+              type="email"
+              name="email"
+              className="input"
+              placeholder="john@email.com"
+              required
+              value={form.email}
+              onChange={handleChange}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+            />
+          </label>
+          <label htmlFor="" className="text-black-500 font-semibold">
+            your message
+            <textarea
+              name="message"
+              rows={4}
+              className="textarea"
+              placeholder="let me know how I can help!"
+              required
+              value={form.message}
+              onChange={handleChange}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+            />
+          </label>
+          <button
+            type="submit"
+            className="btn"
+            onFocus={handleFocus}
+            onBlur={handleBlur}
+            disabled={isLoading}
+          >
+            {isLoading ? "Sending..." : "Send message"}
+          </button>
+        </form>
+      </div>
+    </section>
+  );
 };
 
 export default Contacts;
