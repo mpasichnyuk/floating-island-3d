59332cad9464ba77dd9e886299693bc5a979ab2b
Mikhail <maikl@Mikhails-MacBook-Pro.local>
2024-04-10 18:42:39 -0400

    add emailjs func

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -13,7 +13,18 @@ const Contacts = () => {
     e.preventDefault();
     setIsLoading(true);
 
-    emailjs.sendForm("service_id", "account_id");
+    emailjs.send(
+      import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
+      import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+      {
+        from_name: form.name,
+        to_name: "Mikhail",
+        from_email: form.email,
+        to_email: "mik86@gmail.com",
+        message: form.message,
+      },
+      import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+    );
   };
 
   const handleChange = (e) => {
