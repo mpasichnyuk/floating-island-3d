b0ed30ed078e589f32d0fd16cb0ceb33c88f8784
Mikhail <maikl@Mikhails-MacBook-Pro.local>
2024-04-13 14:11:23 -0400

    start working on About page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,27 @@
 const About = () => {
-  return <div>About</div>;
+  return (
+    <section className="max-container">
+      <h1 className="head-text">
+        Hello I am{" "}
+        <span className="blue-gradient_text font-semibold drop-shadow">
+          Mikhail
+        </span>
+      </h1>
+      <div className="mt-5 flex flex-col gap-3 text-slate-500">
+        <p>
+          Software engineer based in CHarlotte NC, specializing in full stack
+          web development, with experience in motion design, media creation,
+          finance industry, game design and AI
+        </p>
+      </div>
+
+      <div className="py-10 flex flex-col">
+        <h3 className="subhead-text">My skills</h3>
+      </div>
+
+      <div className="mt-16 flex flex-wrap gap-12"></div>
+    </section>
+  );
 };
 
 export default About;
