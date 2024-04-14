8935d11fef34a1120c0a24f206a6ed157c93d801
Mikhail <maikl@Mikhails-MacBook-Pro.local>
2024-04-14 13:58:17 -0400

    fix styling

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { About, Contacts, Home, Projects } from "./pages";
 
 const App = () => {
   return (
-    <main className="bg-slate-300/20 h-[100vh]">
+    <main className="bg-slate-300/20 h-full">
       <Router>
         <Navbar />
         <Routes>
