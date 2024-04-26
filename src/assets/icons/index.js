4032cc83ac6891eeebe574bd991b7bf8c694a1b0
Mikhail Pasichnyuk <m.pasichnyuk@gmail.com>
2024-04-26 14:01:59 -0400

    remove unused icons

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -3,25 +3,12 @@ import git from "./git.svg";
 import github from "./github.svg";
 import html from "./html.svg";
 import javascript from "./javascript.svg";
-import mongodb from "./mongodb.svg";
-import motion from "./motion.svg";
 import mui from "./mui.svg";
 import react from "./react.svg";
 import redux from "./redux.svg";
 import tailwindcss from "./tailwindcss.svg";
-import typescript from "./typescript.svg";
 import linkedin from "./linkedin.svg";
-import twitter from "./twitter.svg";
-import car from "./car.svg";
-import estate from "./estate.svg";
-import pricewise from "./pricewise.svg";
-import snapgram from "./snapgram.svg";
-import summiz from "./summiz.svg";
-import threads from "./threads.svg";
 import arrow from "./arrow.svg";
-import contact from "./contact.svg";
-import soundon from "./soundon.png";
-import soundoff from "./soundoff.png";
 import drag from "./drag.png";
 import game from "./game-8-svgrepo-com.svg";
 import gamecard from "./game-card-svgrepo-com.svg";
@@ -51,23 +38,10 @@ export {
   github,
   html,
   javascript,
-  mongodb,
-  motion,
   mui,
   react,
   redux,
   tailwindcss,
-  typescript,
   linkedin,
-  twitter,
-  car,
-  estate,
-  pricewise,
-  snapgram,
-  summiz,
-  threads,
   arrow,
-  contact,
-  soundon,
-  soundoff,
 };
