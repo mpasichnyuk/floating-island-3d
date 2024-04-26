ea41fe4b7821526533ddcf2eb273e225f1e7841f
Mikhail Pasichnyuk <m.pasichnyuk@gmail.com>
2024-04-26 13:49:40 -0400

    remove unused images

--- src/assets/images/index.js
+++
@@ -1,7 +0,0 @@
-import hero from "./hero.jpg";
-import meta from "./meta.png";
-import shopify from "./shopify.png";
-import tesla from "./tesla.png";
-import logo from "./logo.svg";
-
-export { hero, meta, shopify, tesla, logo };
