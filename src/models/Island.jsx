ace9548f96f134cf4327332fa7ebcc9f9e4d5a6d
Mikhail Pasichnyuk <m.pasichnyuk@gmail.com>
2024-04-25 21:01:02 -0400

    add autorotatin when pressing keyboard keys

--- src/models/Island.jsx
+++ src/models/Island.jsx
@@ -27,6 +27,7 @@ const Island = ({
 }) => {
   // const { isRotating, setIsRotating } = props;
   const islandRef = useRef();
+  const [manualRotation, setManualRotation] = useState(MANUAL_ROTATION);
   const { nodes, materials } = useGLTF(islandScene);
   const { gl, viewport } = useThree();
   const lastX = useRef(0);
@@ -68,21 +69,65 @@ const Island = ({
 
   const handleKeyDown = (e) => {
     if (e.key === "ArrowLeft") {
-      if (!isRotating) setIsRotating(true);
+      if (isRotating) setIsRotating(false);
+      setManualRotation({
+        ...manualRotation,
+        isRotating: true,
+        direction: "left",
+      });
 
-      islandRef.current.rotation.y += 0.005 * Math.PI;
-      rotationSpeed.current = 0.07;
+      // islandRef.current.rotation.y += 0.005 * Math.PI;
+      // rotationSpeed.current = 0.007;
     } else if (e.key === "ArrowRight") {
-      if (!isRotating) setIsRotating(true);
+      if (isRotating) setIsRotating(false);
+      setManualRotation({
+        ...manualRotation,
+        isRotating: true,
+        direction: "right",
+      });
 
-      islandRef.current.rotation.y -= 0.005 * Math.PI;
-      rotationSpeed.current = -0.07;
+      // islandRef.current.rotation.y -= 0.005 * Math.PI;
+      // rotationSpeed.current = -0.007;
     }
   };
 
   const handleKeyUp = (e) => {
     if (e.key === "ArrowLeft" || e.key === "ArrowRight") {
       setIsRotating(false);
+      setManualRotation({
+        ...manualRotation,
+        isRotating: false,
+      });
+    }
+  };
+
+  // Touch events for mobile devices
+  const handleTouchStart = (e) => {
+    e.stopPropagation();
+    e.preventDefault();
+    setIsRotating(true);
+
+    const clientX = e.touches ? e.touches[0].clientX : e.clientX;
+    lastX.current = clientX;
+  };
+
+  const handleTouchEnd = (e) => {
+    e.stopPropagation();
+    e.preventDefault();
+    setIsRotating(false);
+  };
+
+  const handleTouchMove = (e) => {
+    e.stopPropagation();
+    e.preventDefault();
+
+    if (isRotating) {
+      const clientX = e.touches ? e.touches[0].clientX : e.clientX;
+      const delta = (clientX - lastX.current) / viewport.width;
+
+      islandRef.current.rotation.y += delta * 0.01 * Math.PI;
+      lastX.current = clientX;
+      rotationSpeed.current = delta * 0.01 * Math.PI;
     }
   };
 
@@ -92,6 +137,31 @@ const Island = ({
     mesh.geometry.boundingBox.getCenter(center);
     mesh.geometry.center();
     mesh.position.copy(center);
+  };
+
+  const calculateCurrentStage = () => {
+    const rotation = islandRef.current.rotation.y;
+
+    const normalizedRotation =
+      ((rotation % (2 * Math.PI)) + 2 * Math.PI) % (2 * Math.PI);
+
+    // Set the current stage based on the island's orientation
+    switch (true) {
+      case normalizedRotation >= 2.4 && normalizedRotation <= 2.9:
+        setCurrentStage(1);
+        break;
+      case normalizedRotation >= 1.45 && normalizedRotation <= 1.75:
+        setCurrentStage(2);
+        break;
+      case normalizedRotation >= 0 && normalizedRotation <= 0.35:
+        setCurrentStage(3);
+        break;
+      case normalizedRotation >= 4.4 && normalizedRotation <= 4.8:
+        setCurrentStage(4);
+        break;
+      default:
+        setCurrentStage(null);
+    }
   };
 
   useEffect(() => {
@@ -104,6 +174,9 @@ const Island = ({
     canvas.addEventListener("pointermove", handlePointerMove);
     window.addEventListener("keyup", handleKeyUp);
     window.addEventListener("keydown", handleKeyDown);
+    canvas.addEventListener("touchstart", handleTouchStart);
+    canvas.addEventListener("touchend", handleTouchEnd);
+    canvas.addEventListener("touchmove", handleTouchMove);
 
     return () => {
       canvas.removeEventListener("pointerup", handlePointerUp);
@@ -111,58 +184,35 @@ const Island = ({
       canvas.removeEventListener("pointermove", handlePointerMove);
       window.removeEventListener("keyup", handleKeyUp);
       window.removeEventListener("keydown", handleKeyDown);
+      canvas.removeEventListener("touchstart", handleTouchStart);
+      canvas.removeEventListener("touchend", handleTouchEnd);
+      canvas.removeEventListener("touchmove", handleTouchMove);
     };
   }, [gl, handlePointerDown, handlePointerUp, handlePointerMove]);
 
   useFrame(() => {
-    if (!isRotating) {
+    if (!isRotating && !manualRotation.isRotating) {
       rotationSpeed.current *= dampingFactor;
 
       if (Math.abs(rotationSpeed.current) < 0.01) {
         rotationSpeed.current = 0;
       }
       islandRef.current.rotation.y += rotationSpeed.current;
-    } else {
-      const rotation = islandRef.current.rotation.y;
-      /**
-       * this code was copied from Internet
-       * Normalize the rotation value to ensure it stays within the range [0, 2 * Math.PI].
-       * The goal is to ensure that the rotation value remains within a specific range to
-       * prevent potential issues with very large or negative rotation values.
-       *  Here's a step-by-step explanation of what this code does:
-       *  1. rotation % (2 * Math.PI) calculates the remainder of the rotation value when divided
-       *     by 2 * Math.PI. This essentially wraps the rotation value around once it reaches a
-       *     full circle (360 degrees) so that it stays within the range of 0 to 2 * Math.PI.
-       *  2. (rotation % (2 * Math.PI)) + 2 * Math.PI adds 2 * Math.PI to the result from step 1.
-       *     This is done to ensure that the value remains positive and within the range of
-       *     0 to 2 * Math.PI even if it was negative after the modulo operation in step 1.
-       *  3. Finally, ((rotation % (2 * Math.PI)) + 2 * Math.PI) % (2 * Math.PI) applies another
-       *     modulo operation to the value obtained in step 2. This step guarantees that the value
-       *     always stays within the range of 0 to 2 * Math.PI, which is equivalent to a full
-       *     circle in radians.
-       */
-      const normalizedRotation =
-        ((rotation % (2 * Math.PI)) + 2 * Math.PI) % (2 * Math.PI);
-      console.log("normalizedRotation: ", normalizedRotation);
+    } else if (manualRotation.isRotating) {
+      switch (manualRotation.direction) {
+        case "left":
+          islandRef.current.rotation.y += 0.005 * Math.PI;
+          break;
 
-      // Set the current stage based on the island's orientation
-      switch (true) {
-        case normalizedRotation >= 2.4 && normalizedRotation <= 2.9:
-          setCurrentStage(1);
+        case "right":
+          islandRef.current.rotation.y -= 0.005 * Math.PI;
           break;
-        case normalizedRotation >= 1.45 && normalizedRotation <= 1.75:
-          setCurrentStage(2);
-          break;
-        case normalizedRotation >= 0 && normalizedRotation <= 0.35:
-          setCurrentStage(3);
-          break;
-        case normalizedRotation >= 4.4 && normalizedRotation <= 4.8:
-          setCurrentStage(4);
-          break;
+
         default:
-          setCurrentStage(null);
+          break;
       }
     }
+    calculateCurrentStage();
   });
 
   // this useFrame makes Island slowly float up and down
