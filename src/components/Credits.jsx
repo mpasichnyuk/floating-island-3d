5fed3d81781cc82bcdd6572ced4671b724559834
Mikhail Pasichnyuk <m.pasichnyuk@gmail.com>
2024-04-26 15:07:22 -0400

    add credits, fix positioning

--- src/components/Credits.jsx
+++ src/components/Credits.jsx
@@ -6,51 +6,54 @@ const CreditsTooltip = () => {
 
   const creditsText = "Credits";
   const tooltipContent = (
-    <div className="bg-gray-800 text-white p-4 rounded-md shadow-md">
+    <div className="bg-gray-800 text-white p-4 rounded-md shadow-md ">
       <h4 className="text-lg font-bold">Assets Used</h4>
-      <ul>
+      <ul className="text-xs">
         <li>
           {`'Mario Mystery Box' (https://skfb.ly/6QUJu) by planetvfx is licensed
           under Creative Commons Attribution
           (http://creativecommons.org/licenses/by/4.0/).`}
         </li>
-        <li>Asset 2 - License Information</li>
+        <li>{`"Cartoon Plane" (https://skfb.ly/UOLT) by antonmoek is licensed under Creative Commons Attribution (http://creativecommons.org/licenses/by/4.0/).`}</li>
+        <li>{`"Boatrestaurant" (https://skfb.ly/oBWWN) by GiuliaM is licensed under CC Attribution-NonCommercial-NoDerivs (http://creativecommons.org/licenses/by-nc-nd/4.0/).`}</li>
+        <li>{`"phoenix bird" (https://skfb.ly/6vLBp) by NORBERTO-3D is licensed under Creative Commons Attribution (http://creativecommons.org/licenses/by/4.0/).`}</li>
       </ul>
     </div>
   );
 
-  useEffect(() => {
-    const handleResize = () => {
-      const tooltip = tooltipRef.current;
-      if (tooltip) {
-        const tooltipRect = tooltip.getBoundingClientRect();
-        const viewportWidth = window.innerWidth;
-        const viewportHeight = window.innerHeight;
+  // useEffect(() => {
+  //   const handleResize = () => {
+  //     const tooltip = tooltipRef.current;
+  //     if (tooltip) {
+  //       const tooltipRect = tooltip.getBoundingClientRect();
+  //       const viewportWidth = window.innerWidth;
+  //       const viewportHeight = window.innerHeight;
 
-        const isBottomVisible = viewportHeight - tooltipRect.bottom > 0;
-        const isRightVisible = viewportWidth - tooltipRect.right > 0;
+  //       const isBottomVisible = viewportHeight - tooltipRect.bottom > 0;
+  //       const isRightVisible = viewportWidth - tooltipRect.right > 0;
 
-        tooltip.style.top = isBottomVisible ? "auto" : "-100%";
-        tooltip.style.bottom = isBottomVisible ? "100%" : "auto";
-        tooltip.style.left = isRightVisible ? "auto" : "0";
-        tooltip.style.right = isRightVisible ? "0" : "auto";
-      }
-    };
+  //       tooltip.style.top = isBottomVisible ? "auto" : "-100%";
+  //       tooltip.style.bottom = isBottomVisible ? "100%" : "auto";
+  //       tooltip.style.left = isRightVisible ? "auto" : "0";
+  //       tooltip.style.right = isRightVisible ? "0" : "auto";
+  //     }
+  //   };
 
-    window.addEventListener("resize", handleResize);
-    handleResize();
+  //   window.addEventListener("resize", handleResize);
+  //   handleResize();
 
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, [showTooltip]);
+  //   return () => {
+  //     window.removeEventListener("resize", handleResize);
+  //   };
+  // }, [showTooltip]);
 
   return (
-    <div className="fixed bottom-2 right-2">
+    <div className="fixed bottom-2 right-2  z-20 ">
       <div
-        onMouseEnter={() => setShowTooltip(true)}
-        onMouseLeave={() => setShowTooltip(false)}
-        className="relative inline-block"
+        // onMouseEnter={() => setShowTooltip(true)}
+        // onMouseLeave={() => setShowTooltip(false)}
+        onClick={() => setShowTooltip(!showTooltip)}
+        className=""
       >
         <span className="text-sm cursor-pointer text-slate-400	 hover:underline">
           {creditsText}
@@ -58,7 +61,7 @@ const CreditsTooltip = () => {
         {showTooltip && (
           <div
             ref={tooltipRef}
-            className=" bottom-10 right-5 z-11 bg-gray-800 text-slate-400	 p-4 rounded-md shadow-md"
+            className=" bg-gray-800 text-slate-400	 p-4 rounded-md shadow-md "
           >
             {tooltipContent}
           </div>
